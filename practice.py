7478f78c53f4df88d787f896bc6edb1bf284eeb6
ticochuck <chucklivilla@gmail.com>
2021-07-06 20:17:05 -0700

    csv and other functions

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 import re
+import csv
 
 file_name = 'potential-contacts.txt'
 
@@ -13,10 +14,32 @@ try:
         #         print('i found a number!: ', x)
         phones = re.findall(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})', contents)
 
-        for phone in phones:
-            print('Phone:', phone)
+        # for phone in phones:
+        #     print('Phone:', phone)
         
 
 except FileNotFoundError as err:
     print('i found an error\n', err)
 
+
+def csvs():
+
+    file_name = 'test_propellers.csv'
+
+    try:
+        with open(file_name) as info:
+            contents = csv.reader(info, delimiter=',')
+            line = 0
+            for row in contents:
+                if line == 0:
+                    print(f'headers are: {row}')
+                    line += 1
+                else:
+                    print(f'data is {row}')
+            # print(type(contents))
+            # for i in contents[:10]:
+            #     print(i)
+    except FileNotFoundError as err:
+        print("Went to exeption, err= \n", err)
+
+csvs()
